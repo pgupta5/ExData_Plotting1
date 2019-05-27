8e50538cb054300518a58e018712e954fd4adac9
pgupta5 <50067206+pgupta5@users.noreply.github.com>
2019-05-27 14:12:55 -0400

    Updated

--- Plot2.R
+++ Plot2.R
@@ -8,9 +8,9 @@ df$Date <- as.Date(df$Date,"%d/%m/%Y")
 #filter data for the given date range
 df <- filter(df,Date == as.Date("2007-2-1") | Date == as.Date("2007-2-2"))
 #Creating and adding new column
-within(df1, dateTime <- paste(df1$Date, df1$Time))
-df1$dateTime <- as.POSIXct(dateTime)
+within(df, dateTime <- paste(df$Date, df$Time))
+df$dateTime <- as.POSIXct(dateTime)
 #plot2
 dev.copy(png,'plot2.png', width=480, height=480)
-plot(df1$Global_active_power~df1$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+plot(df$Global_active_power~df$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
 dev.off()
